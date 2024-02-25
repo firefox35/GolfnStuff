80b2807d962750121d5dcf3e6a14f67e97959191
Damien McLoughlin <f1firefox@yahoo.ie>
2024-02-25 15:42:44 +0000

    add contact form

--- contact/urls.py
+++ contact/urls.py
@@ -1,8 +1,7 @@
-from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('contact.urls')),
+    path('contact/', views.contact, name='contacts'),
 ]
