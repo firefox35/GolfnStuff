b7092afb4de560cd871a562a5824718ef97c2542
Damien McLoughlin <f1firefox@yahoo.ie>
2024-03-18 16:15:21 +0000

    added background image to product pages

--- golfnstuff/settings.py
+++ golfnstuff/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = False
+
+'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['8000-firefox35-golfnstuff-la2cdtf2lur.ws-eu110.gitpod.io','golfnstuff-ee9465ae2158.herokuapp.com']
 
