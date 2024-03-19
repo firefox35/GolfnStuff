9e7ba6db8067556b3c5b5aa34ed3b9ee9a66b6e5
Damien McLoughlin <f1firefox@yahoo.ie>
2024-03-19 00:19:19 +0000

    Add load static to review and change code in lessons

--- lessons/views.py
+++ lessons/views.py
@@ -8,22 +8,15 @@ from django.http import HttpResponse
 
 def lesson(request):
     if request.method=="POST":
-            lesson=Lessons()
-            name=request.POST.get('name')
-            email=request.POST.get('email')
-            phone=request.POST.get('phone')
-            golfer=request.POST.get('golfer')
-            date=request.POST.get('date')
-            time=request.POST.get('time')
-            comment=request.POST.get('comment')
-            lesson.name=name
-            lesson.email=email
-            lesson.golfer=golfer
-            lesson.phone=phone
-            lesson.date=date
-            lesson.time=time
-            lesson.comment=comment
-            lesson.save()
+            new_lesson=Lessons()
+            new_lesson.name=request.POST.get('name')
+            new_lesson.email=request.POST.get('email')
+            new_lesson.phone=request.POST.get('phone')
+            new_lesson.golfer=request.POST.get('golfer')
+            new_lesson.date=request.POST.get('date')
+            new_lesson.time=request.POST.get('time')
+            new_lesson.comment=request.POST.get('comment')
+            new_lesson.save()
             return render(request,'lesson/lesson.html')
     return render(request, 'lesson/lessons.html')
 
