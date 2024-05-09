08f2d5e57d198b30860a50bd25977ee627c7528a
Damien McLoughlin <f1firefox@yahoo.ie>
2024-05-09 23:52:17 +0000

    fix another link in checkout models

--- checkout/models.py
+++ checkout/models.py
@@ -77,9 +77,9 @@ class OrderLineItem(models.Model):
     product_size = models.CharField(max_length=2, null=True,
                                     blank=True)  # XS, S, M, L, XL
     quantity = models.IntegerField(null=False, blank=False, default=0)
-    lineitem_total =
-    models.DecimalField(max_digits=6, decimal_places=2, null=False,
-                        blank=False, editable=False)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2,
+                                         null=False, blank=False,
+                                         editable=False)
 
     def save(self, *args, **kwargs):
         """
